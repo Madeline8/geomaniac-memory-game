d19eb702e55ad5d2ea42c4c96c4dc0e5cd11a5b1
Magda <madeline@gmx.co.uk>
2021-08-13 19:41:58 +0000

    Made changes to script.js to show and hide divs in a much simpler way, rewrote CloseAllModals function, as there were too many divs to manage and changed it to use an array, so the code is much cleaner. Linked ready-play button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,29 @@ function log(txt) {
 document.getElementById("btn-play").addEventListener("click", () => {
   soundClick();
   closeAllModals();
-  document.getElementById("div-difficulty").classList.remove("invisible");
-  document.getElementById("div-difficulty").classList.add("visible");
+  showHide(true, 'div-difficulty');
 });
 
 // Play --> Choose difficulty level -> easy
 document.getElementById("easy-level-button").addEventListener("click", event => {
   soundClick();
   closeAllModals();
-  document.getElementById("easy-level-game").classList.remove("invisible");
-  document.getElementById("easy-level-game").classList.add("visible");
+  showHide(true, 'easy-level-game');
 });
 
 // Difficulty level - Go Back
 document.getElementById("diff-level-go-back").addEventListener("click", event => {
   log("diff level.go back clicked");
   soundClick();
-  document.getElementById("div-difficulty").classList.remove('visible');
-  document.getElementById("div-difficulty").classList.add('invisible');
-  document.getElementById("home-page-modal").classList.remove('invisible');
-  document.getElementById("home-page-modal").classList.add('visible');
+  showHide(false, 'div-difficulty');
+  showHide(true, 'home-page-modal');
 });
 
 // Rules Button
 document.getElementById("btn-rules").addEventListener("click", event => {
   soundClick();
   closeAllModals();
-  document.getElementById("div-rules").classList.remove("invisible");
-  document.getElementById("div-rules").classList.add("visible");
+  showHide(true, 'div-rules');
 
 });
 
@@ -44,25 +39,22 @@ document.getElementById("btn-rules").addEventListener("click", event => {
 document.getElementById("btn-rules-back").addEventListener("click", event => {
   log("btn-rules-back calling sound click");
   soundClick();
-  document.getElementById("div-rules").classList.add("invisible");
-  document.getElementById("home-page-modal").classList.remove('invisible');
-  document.getElementById("home-page-modal").classList.add('visible');
+  showHide(false, 'div-rules');
+  showHide(true, 'home-page-modal');
 });
 
 // Sounds Button
 document.getElementById("btn-sounds").addEventListener("click", event => {
   soundClick();
   closeAllModals();
-  document.getElementById("div-sounds").classList.remove("invisible");
-  document.getElementById("div-sounds").classList.add("visible");
+  showHide(true, 'div-sounds');
 });
 
 // Sound form Go Back
 document.getElementById("btn-sound-back").addEventListener("click", event => {
   soundClick();
-  document.getElementById("div-sounds").classList.add('invisible');
-  document.getElementById("home-page-modal").classList.remove('invisible');
-  document.getElementById("home-page-modal").classList.add('visible');
+  showHide(false, 'div-sounds');
+  showHide(true, 'home-page-modal');
 });
 
 // Contact Us Button
@@ -80,16 +72,26 @@ document.getElementById("btn-sound-back").addEventListener("click", event => {
 
 
 function closeAllModals() {
-  document.getElementById("home-page-modal").classList.remove('visible');
-  document.getElementById("home-page-modal").classList.add('invisible');
-  document.getElementById("div-rules").classList.remove('visible');
-  document.getElementById("div-rules").classList.add('invisible');
-  document.getElementById("div-sounds").classList.remove('visible');
-  document.getElementById("div-sounds").classList.add('invisible');
-  // document.getElementById("contact-page-window").classList.remove('visible');
-  // document.getElementById("contact-page-window").classList.add('invisible');
-  document.getElementById("div-difficulty").classList.remove('visible');
-  document.getElementById("div-difficulty").classList.add('invisible');
+  //let divs = [ 'home-page-modal', 'div-rules', 'div-sounds', 'contact-page-window',  'div-difficulty' ];
+  let divs = [ 'home-page-modal', 'div-rules', 'div-sounds', 'div-difficulty' ];
+  for(let i = 0; i < divs.length; i++)
+    showHide(false, divs[i]);
+}
+
+// Show or hide a div (show == true will show it, show == false will hide it)
+function showHide(show, div) {
+  let elm = document.getElementById(div);
+  if(!elm) {
+    log("Cannot set element - not found; " + div);
+    return;
+  }
+  if(show) {
+    elm.classList.remove('invisible');
+    elm.classList.add('visible');
+  } else {
+    elm.classList.remove('visible');
+    elm.classList.add('invisible');
+  }
 }
 
 // Audio and music settings
@@ -102,18 +104,3 @@ document.getElementById("music-btn").addEventListener("click", event => {
   soundClick();
   musicManager();
 });
-
-
-// Logic for starting the game
-
-// logic for the timer
-
-//display of the results
-
-//logic for number of retries 
-
-// saving results and for showing the results table
-
-//Audio related functions
-
-
